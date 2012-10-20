a7aed47c4fd42e6de696073f68bfd9b6ac15e2f4
Mitch Lloyd <mitch.lloyd@gmail.com>
2012-10-20 19:45:38 -0400

    Move the form into the submissions new page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 VacantProperties::Application.routes.draw do
 
-  resources :submissions, only: [:create, :show]
+  resources :submissions
   root to: 'high_voltage/pages#show', id: 'home'
   match "/thank_you/:id" => "submissions#show"
 
