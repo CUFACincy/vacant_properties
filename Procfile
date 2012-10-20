b059403ae2ad28efef3d5268003527dde47e1bf3
Chris Moore <cdmwebs@gmail.com>
2012-10-20 03:13:12 -0400

    set up resque

--- Procfile
+++ Procfile
@@ -1,2 +1,5 @@
+redis: redis-server $REDIS_CONF
+db: postgres -D pg
 web: bundle exec thin start -p $PORT
+worker: rake resque:work QUEUE=*
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
+require 'resque/server'
+
 VacantProperties::Application.routes.draw do
 
+  mount Resque::Server.new, :at => "/resque"
   resources :submissions, only: :create
   root to: 'high_voltage/pages#show', id: 'home'
 
