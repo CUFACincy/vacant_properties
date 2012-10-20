a6bffbcd62ad6d8ab912ffe5a9ec6db7495d62de
Chris Moore <cdmwebs@gmail.com>
2012-10-20 10:45:58 -0400

    remove resque route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
-require 'resque/server'
-
 VacantProperties::Application.routes.draw do
 
-  mount Resque::Server.new, :at => "/resque"
   resources :submissions, only: :create
   root to: 'high_voltage/pages#show', id: 'home'
 
