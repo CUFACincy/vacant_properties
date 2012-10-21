8779f17dd979cd3fc02ae67299146f68db2a5e1b
Chris Moore <cdmwebs@gmail.com>
2012-10-21 17:34:09 -0400

    show the wufoo entry id on admin index

--- app/admin/submissions.rb
+++ app/admin/submissions.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register Submission do
-  
+  index do
+    column :id do |submission|
+      link_to submission.id, admin_submission_path(submission)
+    end
+    column :wufoo_entry_id
+    column :created_at
+    column :updated_at
+  end
 end
