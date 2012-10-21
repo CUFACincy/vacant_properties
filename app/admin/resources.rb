1926d8e2a1cbb17f37a57146c9b9933bbcc39294
Chris Moore <cdmwebs@gmail.com>
2012-10-21 15:19:37 -0400

    fix active_admin association

--- app/admin/resources.rb
+++ app/admin/resources.rb
@@ -1,4 +1,24 @@
 ActiveAdmin.register Resource do
+  show do |ad|
+    attributes_table do
+      row :id
+      row :name
+      row :phone
+      row :phone_extension
+      row :department_name
+      row :contact_name
+      row :contact_email
+      row :other_information
+      row :locality
+      row :created_at
+      row :updated_at
+      row :complaints do
+        resource.complaints.map(&:name).join(', ')
+      end
+    end
+    active_admin_comments
+  end
+
   form do |f|
     f.inputs do
       f.input :name
@@ -9,7 +29,7 @@ ActiveAdmin.register Resource do
       f.input :contact_email
       f.input :other_information
       f.input :locality
-      f.input :complaints, as: 'check_boxes', collection: Complaint.all.map { |c| [c.name, c.id] }
+      f.input :complaints
     end
     f.buttons
   end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,5 +1,7 @@
 class Resource < ActiveRecord::Base
-  attr_accessible :name, :phone, :phone_extension, :department_name, :contact_name, :contact_email, :locality_id
+  attr_accessible :name, :phone, :phone_extension, :department_name,
+    :contact_name, :contact_email, :locality_id, :complaint_ids,
+    :other_information
   belongs_to :locality
   has_and_belongs_to_many :complaints
 end
