16ae0917bfc028068843fcbb11950c216716f7d7
Mitch Lloyd <mitch.lloyd@gmail.com>
2012-10-21 14:25:38 -0400

    Make the resource complaints editable.

--- app/admin/resources.rb
+++ app/admin/resources.rb
@@ -1,3 +1,16 @@
 ActiveAdmin.register Resource do
-  
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :phone
+      f.input :phone_extension
+      f.input :department_name
+      f.input :contact_name
+      f.input :contact_email
+      f.input :other_information
+      f.input :locality
+      f.input :complaints, as: 'check_boxes', collection: Complaint.all.map { |c| [c.name, c.id] }
+    end
+    f.buttons
+  end
 end
